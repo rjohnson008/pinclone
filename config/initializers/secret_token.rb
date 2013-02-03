f5c9854eeaa9109a2551f629960b5bab5ef35cdf
Ryan Johnson <rjohnson008@yahoo.com>
2013-02-03 09:12:02 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinclone::Application.config.secret_token = '9b0f63f4646e693466446245e4113f926854ed2e7304506f309eaf5a90f7778f0189988cf860bb5fad7327d8694ae20c64e215b471545dd78315e7513348b850'
