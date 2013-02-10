c8b49e9ada75e100cc0a21b11c9b603ccdb68f7f
Ryan Johnson <rjohnson008@yahoo.com>
2013-02-10 13:00:24 -0800

    Add User model with Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Pinclone::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # URL included in outbound email to users
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pinclone::Application.routes.draw do
+  devise_for :users
+
   get 'about' => 'pages#about'
 
   root :to => 'pages#home'
